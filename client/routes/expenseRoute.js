64b74f656e419f501f7d7184110d5dc5a53f0cd6
Jagadeesh <jagadeeshmerugumala0747@gmail.com>
2026-05-30 23:42:51 +0530

    removed old backend code

--- client/routes/expenseRoute.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const expenseController = require('../controllers/expenseController');
-
-router.post('/add', expenseController.addExpense);
-router.get('/group/:groupId', expenseController.getExpensesByGroup);
-
-module.exports = router;
--- client/routes/groupRoute.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const { createGroup, getUserGroups } = require('../controllers/groupController');
-
-router.post('/create', createGroup);
-router.get('/user-groups/:userId', getUserGroups);
-
-module.exports = router;
